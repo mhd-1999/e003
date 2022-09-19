b02ac9c630850a4f25975c180566933991ace6fb
mhd-1999 <maihaiduong2015@gmail.com>
2022-09-19 11:43:52 +0700

    update-final

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="list">
+      <ListTree />
+    </div>
+  </div>
+</template>
+
+<script>
+import ListTree from "./components/ListTree.vue";
+export default {
+  name: "App",
+  components: { ListTree },
+};
+</script>
+
+<style>
+body {
+  background: white;
+}
+body::-webkit-scrollbar {
+  display: none;
+}
+</style>
